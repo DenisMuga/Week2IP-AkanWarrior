ed2e164697f3ff5491684000e7036f3f1da86568
DenisMuga <denismugah5@gmail.com>
2022-03-05 06:08:03 +0300

    chore: adding a collection - adding a collection for the male names

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 var DaysOfTheWeek = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+var AkanNames = {
+    "male": {
+        "sunday": "Kwasi",
+        "monday": "Kwadwo",
+        "tuesday": "Kwabena",
+        "wednesday": "Kwaku",
+        "thursday": "Yaw",
+        "friday": "Kofi",
+        "saturday": "Kwame"
+    }
+}
